b3c7496da7549d6735578af105ee4528d5b3c06c
srujanm13 <50265279+srujanm13@users.noreply.github.com>
2026-08-03 21:43:48 +0530

    Update testcase1.spec.js

--- tests/testcase1.spec.js
+++ tests/testcase1.spec.js
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
 import { login,logout } from '../helper/sitehelpers.js';
 test('login test', async ({ page }) => {
-  console.log('username:', process.env.username);
-  console.log('password:', process.env.password);
+  console.log('username:', process.env.USERNAME);
+  console.log('password:', process.env.PASSWORD);
   await login(page, process.env.username, process.env.password);
   await expect(page.locator('h1')).toContainText('Dashboard');
   await logout(page);
