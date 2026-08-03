ad6aa9a79e99b7f8de3b03903a950e55a107ae0a
srujanm13 <50265279+srujanm13@users.noreply.github.com>
2026-08-03 12:48:35 +0530

    Update Test.spec.js

--- tests/Test.spec.js
+++ tests/Test.spec.js
@@ -1,7 +1,2 @@
-import {test,expect} from '@playwright/test';
-
-test('Test', async ({page}) => {
-  await page.goto('https://eventhub.rahulshettyacademy.com/');
-  const title = await page.title();
-  expect(title).toBe('EventHub — Discover & Book Events');
-});
+console.log(process.env.QA.BASEURL)
+console.log(process.env.QA.BROWSER)
